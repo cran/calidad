c761dd3fc8d2e9a50552ba118dca3f9760ca89f5
Klaus Lehmann <klehmann@fen.uchile.cl>
2025-04-15 15:10:02 +0000

    version 0.8.0

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,8 @@
 
-# calidad 0.7.0
+# calidad 0.8.0
 
-- Logistic coefficient of variation for proportions  
+- New quality standard (INE Chile economics, 2024) 
 
+- New parameter ratio_between_0_1 in assess function to improve the assessment for quality in the case of proportions.  
 
- 
+- New step in CEPAL 2023 schema to include the case of justified low degrees of freedom. Implemented trough low_df_justified parameter   
--- inst/doc/tutorial.R
+++ inst/doc/tutorial.R
@@ -8,10 +8,13 @@ knitr::opts_chunk$set(
 library(survey)
 library(calidad)
 library(dplyr)
+
 ene <- ene %>% 
   mutate(fdt = if_else(cae_especifico >= 1 & cae_especifico <= 9, 1, 0), # labour force
          ocupado = if_else(cae_especifico >= 1 & cae_especifico <= 7, 1, 0), # employed
-         desocupado = if_else(cae_especifico >= 8 & cae_especifico <= 9, 1, 0)) # unemployed
+         desocupado = if_else(cae_especifico >= 8 & cae_especifico <= 9, 1, 0),
+         hombre = if_else(sexo == 1, 1, 0),
+         mujer = if_else(sexo == 2, 1, 0)) # unemployed
 
 # One row per household
 epf <- epf_personas %>% 
@@ -33,11 +36,19 @@ dc_epf <- svydesign(ids = ~varunit, strata = ~varstrat, data = epf, weights = ~f
 
 options(survey.lonely.psu = "certainty")
 
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  # Complex sample design for ELE
+#  dc_ele <- svydesign(ids = ~rol_ficticio, weights = ~fe_transversal, strata = ~estrato, fpc = ~pob, data = ELE7)
+#  
+#  options(survey.lonely.psu = 'remove')
+#  
+
 ## -----------------------------------------------------------------------------
 insumos_prop <- create_prop(var = "desocupado", domains = "sexo", subpop = "fdt", design =  dc_ene) # proportion of unemployed people
 insumos_total <-  create_size(var = "desocupado", domains = "sexo", subpop = "fdt", design =  dc_ene) # number of unemployed people
 
-## ---- include=F---------------------------------------------------------------
+## -----------------------------------------------------------------------------
 insumos_total
 
 ## -----------------------------------------------------------------------------
@@ -46,6 +57,13 @@ desagregar <- create_prop(var = "desocupado", domains = "sexo+region", subpop = 
 ## ---- eval=F------------------------------------------------------------------
 #  
 #  eclac_inputs <-  create_prop(var = "desocupado", domains = "sexo+region", subpop = "fdt", design =  dc_ene, eclac_input = TRUE)
+#  
+
+## ---- eval=FALSE, warning=FALSE-----------------------------------------------
+#  
+#  create_prop(var = "mujer", denominator = "hombre", domains = "ocupado", design = dc_ene,
+#              eclac_input = TRUE, scheme = 'eclac_2023')
+#  
 
 ## ---- eval=T, warning=FALSE---------------------------------------------------
 insumos_suma <-  create_total(var = "gastot_hd", domains = "zona", design =  dc_epf)
@@ -57,6 +75,7 @@ insumos_media <-  create_mean(var = "gastot_hd", domains = "zona", design =  dc_
 # ENE dataset
 insumos_prop_nacional <- create_prop("desocupado", subpop = "fdt", design = dc_ene)
 insumos_total_nacional <-  create_total("desocupado", subpop = "fdt", design = dc_ene)
+
 # EPF dataset
 insumos_suma_nacional <- create_total("gastot_hd", design = dc_epf)
 insumos_media_nacional <-  create_mean("gastot_hd", design = dc_epf)
@@ -66,6 +85,10 @@ insumos_media_nacional <-  create_mean("gastot_hd", design = dc_epf)
 prop_nacional_ci <- create_prop("desocupado", subpop = "fdt", design = dc_ene, ci = TRUE)                        
 prop_nacional_ci_logit <- create_prop("desocupado", subpop = "fdt", design = dc_ene, ci_logit = TRUE) 
 
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  
+#  prod_salarial <- create_prop('VA_2022f', denominator = 'REMP_TOTAL', domains = 'cod_actividad+cod_tamano', design = dc_ele)
 
 ## ---- eval=F, warning=FALSE---------------------------------------------------
 #  
@@ -77,7 +100,23 @@ prop_nacional_ci_logit <- create_prop("desocupado", subpop = "fdt", design = dc_
 #  
 #  # ECLAC
 #  evaluacion_cepal_2020 <- assess(eclac_inputs, scheme = 'eclac_2020')
-#  evaluacion_cepal_2023 <- assess(eclac_inputs, scheme = 'eclac_2023')
+#  evaluacion_cepal_2023 <- assess(eclac_inputs, scheme = 'eclac_2023', domain_info = TRUE, low_df_justified = TRUE)
+#  
+#  
+
+## ---- eval = FALSE------------------------------------------------------------
+#  # INE Economics
+#  
+#  ## target sample size
+#  table_n_obj <- ELE7_n_obj %>%
+#    dplyr::mutate(cod_actividad = cod_actividad_letra,
+#                  cod_tamano = as.character(cod_tamano)) %>%
+#    dplyr::select(-cod_actividad_letra)
+#  
+#  
+#  eval_ratio <- assess(prod_salarial, scheme = 'chile_economics',
+#                       domain_info = TRUE, table_n_obj = table_n_obj, ratio_between_0_1 = FALSE)
+#  
 
 ## ----eval=F-------------------------------------------------------------------
 #  # Unemployment by region
